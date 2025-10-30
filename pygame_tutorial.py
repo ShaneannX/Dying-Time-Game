ba157a90deca6f5ee4fc07702d4e473b4a400186
ShaneannX <shaneann.ahtack@gmail.com>
2025-10-30 11:35:04 +0000

    Added a starter menu

--- pygame_tutorial.py
+++ pygame_tutorial.py
@@ -4,33 +4,63 @@ from sys import exit
 pygame.init() # Initialize Pygame
 
 # Set up display
-screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption("Let's go")
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Dying Time")
 
 Clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf',50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/Ground.png')
-text_surface = test_font.render('My game', False, 'black')
+closed_door_surface = pygame.image.load('graphics\door\closed_door.png')
+opened_door_surface = pygame.image.load('graphics\door\opened_door.png')
+play_button_surface = pygame.image.load('graphics\door\start_menu\play_button.png')
+font = pygame.font.Font('font/Pixeltype.ttf', 50)
+closed_door_rect = closed_door_surface.get_rect(topleft=(100,100))
+start_button_rect = play_button_surface.get_rect(topleft = (350,200))
+door_opened = False
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
+def start_menu():
+    running = True
 
-snail_movement = 750
+    while running:
+        screen.fill((30,30,30))
+        title_text = font.render("Dying Time", True, (255, 255, 255))
+        screen.blit(title_text, (325, 100))
+        screen.blit(play_button_surface, start_button_rect)
+        pygame.display.update()
+
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if start_button_rect.collidepoint(event.pos):
+                        running = False  # Exit menu and start game
+
+
+start_menu()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if closed_door_rect.collidepoint(event.pos):
+                door_opened = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.mod & pygame.KMOD_CAPS:
+                door_opened = False
     # draw all our elements here
     # update everything
 
-    screen.blit(sky_surface,(0,0)) # Block image transfer
-    screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface,(300,50))
-    snail_movement -= 4
-    if snail_movement < -100: snail_movement = 800
-    screen.blit(snail_surface,(snail_movement,275))
+    screen.fill('grey') # Block image transfer
+    
+    if door_opened:
+        screen.blit(opened_door_surface, (215,100))
+    else:
+        screen.blit(closed_door_surface, (215,100))
 
     pygame.display.update()  # Update the display
     Clock.tick(60) # Max frameRate
